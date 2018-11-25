fdf132bdb50d57d5c7d98a99c31dfbd901c09e72
harhal19 <harhal19@mail.ru>
2018-11-25 09:30:54 +0300

    Semafor not ready, I'm too tired to do it

---
+++ Assets/SCPCrimeScene/Scripts/InteractiveElements/Semafor/SequenceController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SequenceState
+{
+	Inactive,
+	Preview,
+	Input,
+	Completed,
+	Failed
+}
+
+public class SequenceController : MonoBehaviour
+{
+	public SequenceElement[] SequenceElements;
+
+	[SerializeField] protected int StartSequenceLength = 1;
+
+	[SerializeField] protected int MaxSequenceLength = 5;
+
+	private List<SequenceElement> CurrentSequence;
+
+	private int CurrentSequenceLength;
+
+	private int FilledSequenceLength;
+
+	public SequenceState CurrentState { get; private set; }
+
+	void ResetSequence()
+	{
+		CurrentSequenceLength = 1;
+		GenerateSequence(); 
+		               
+		PlaySequencePreview();
+	}
+
+	void GenerateSequence()
+	{
+		CurrentSequence = new List<SequenceElement>();
+		for (int i = 0; i < MaxSequenceLength; i++)
+		{
+			CurrentSequence.Add(SequenceElements[Random.Range(0, SequenceElements.Length)]);
+		}
+	}
+
+	void PlaySequencePreview()
+	{
+		CurrentState = SequenceState.Preview;
+	}
+
+	void OnPreviewFinished()
+	{
+		FilledSequenceLength = 0;
+		CurrentState = SequenceState.Input;
+	}
+
+	public bool OnSequenceElementInput(SequenceElement sequenceElement)
+	{
+		if (CurrentState != SequenceState.Input)
+		{
+			return false;
+		}
+
+		if (CurrentSequence[FilledSequenceLength] == sequenceElement)
+		{ 
+			FilledSequenceLength++;
+			if (FilledSequenceLength >= CurrentSequenceLength)
+			{
+				OnSequencedCompleted();
+			}
+
+			return true;
+		}
+		else
+		{
+			CurrentState = SequenceState.Failed;
+			return false;
+		}
+	}
+
+	void OnSequencedCompleted()
+	{
+		FilledSequenceLength = 0;
+		CurrentSequenceLength++;
+		if (CurrentSequenceLength > MaxSequenceLength)
+		{
+			Open();
+		}
+		else
+		{
+			PlaySequencePreview();
+		}
+	}
+
+	void Open()
+	{
+		enabled = false;
+	}
+}
---
+++ Assets/SCPCrimeScene/Scripts/InteractiveElements/Semafor/SequenceElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceElement : MonoBehaviour//, IInteractiveObject
+{
+    /*public SequenceController ElementOwner;
+
+    public void Highlight(Color color)
+    {
+        
+    }
+
+    public void Press()
+    {
+        if (ElementOwner.OnSequenceElementInput(this)) 
+        {
+            Highlight(Color.green);
+        }
+        else
+        {
+            Highlight(Color.red);
+        }
+    }
+    
+    public void InteractWithObject(InteractionType interactionType, HitInfo hitInfo)
+    {
+        if (interactionType == InteractionType.Pressed)
+        {
+            Press();
+        }
+    }
+
+    public bool IsEnabled()
+    {
+        return enabled && ElementOwner.CurrentState == SequenceState.Input;
+    }
+
+    public bool ValidateNormalOffset(float normalOffsetCos)
+    {
+        return normalOffsetCos > 0.3f;
+    }*/
+}
